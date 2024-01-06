c65489c24347b82cebd1de0849baa4d6006128a4
Marcos Souza <markin.mv.amaral@gmail.com>
2024-01-06 12:05:59 -0300

    chore: Remove unnecessary assertions

--- application/src/test/java/com/fiap/burger/application/bdd/CucumberIntegrationTest.java
+++ application/src/test/java/com/fiap/burger/application/bdd/CucumberIntegrationTest.java
@@ -2,8 +2,6 @@ package com.fiap.burger.application.bdd;
 
 import com.fiap.burger.application.config.CucumberTestConfig;
 import io.cucumber.spring.CucumberContextConfiguration;
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
@@ -22,9 +20,4 @@ public class CucumberIntegrationTest {
     @LocalServerPort
     protected int port;
 
-    @BeforeEach
-    public void setup() throws Exception {
-        RestAssured.port = port;
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-    }
 }
