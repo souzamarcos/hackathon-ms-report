b6033283cafbf21ae253fa1a4e69a80a4c8c7b5a
Guilherme Oliveira <ghvoliveira@gmail.com>
2024-03-21 20:13:15 -0300

    Fix Testes de token e auth

--- api/src/test/java/com/fiap/hackathon/api/misc/token/TokenJwtUtilsTest.java
+++ api/src/test/java/com/fiap/hackathon/api/misc/token/TokenJwtUtilsTest.java
@@ -2,13 +2,19 @@ package com.fiap.hackathon.api.misc.token;
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fiap.hackathon.entity.employee.Employee;
+import com.fiap.hackathon.entity.employee.EmployeeType;
 import com.fiap.hackathon.usecase.misc.secret.SecretUtils;
 import com.fiap.hackathon.usecase.misc.secret.TokenJwtSecret;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -30,32 +36,60 @@ class TokenJwtUtilsTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    @Test
-    void readTokenOkay() {
-        var expected = "ABC001";
-        String validToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBdWRpZW5jZSIsImlzcyI6IlRFU1QtSVNTVUVSIiwiaWQiOiJBQkMwMDEiLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5hbWUiOiJOb21lIiwidHlwZSI6MCwiaWF0IjoxNzA2NDEzNDMyLCJqdGkiOiJmMDQyM2U0Yy03YzEwLTRiMzUtOGQ5NS1kYmZhZmMzY2ZkYTQifQ._yL06ZjOO0k2Iin1udWAeou-MYVukcZ9XVOsJScVNiU";
+    @Nested
+    class readToken {
+        @Test
+        void readTokenOkay() {
+            var expected = "ABC001";
+            String validToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBdWRpZW5jZSIsImlzcyI6IlRFU1QtSVNTVUVSIiwiaWQiOiJBQkMwMDEiLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5hbWUiOiJOb21lIiwidHlwZSI6MCwiaWF0IjoxNzA2NDEzNDMyLCJqdGkiOiJmMDQyM2U0Yy03YzEwLTRiMzUtOGQ5NS1kYmZhZmMzY2ZkYTQifQ._yL06ZjOO0k2Iin1udWAeou-MYVukcZ9XVOsJScVNiU";
 
-        when(secretUtils.getTokenJwtSecret()).thenReturn(new TokenJwtSecret(TOKEN_SECRET, TOKEN_ISSUER));
+            when(secretUtils.getTokenJwtSecret()).thenReturn(new TokenJwtSecret(TOKEN_SECRET, TOKEN_ISSUER));
 
-        DecodedJWT result = tokenJwtUtils.readToken(validToken);
-        assertEquals(expected, result.getClaim("id").asString());
+            DecodedJWT result = tokenJwtUtils.readToken(validToken);
+            assertEquals(expected, result.getClaim("id").asString());
+        }
+
+        @Test
+        void readTokenExpired() {
+            String expiredToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBdWRpZW5jZSIsImNsaWVudElkIjoxLCJpc3MiOiJURVNULUlTU1VFUiIsImNwZiI6IjE2NTY1ODI0NzM4IiwiZXhwIjoxNjk4NjI4MjE2LCJpYXQiOjE2OTg2MjgxNTYsImp0aSI6ImRmYjM1OWFjLTI3MGItNDUxMC05YjcxLTZiYjE3YzI5MzQ1NCJ9.iVcSUx-UShDr1rSSJt2lweRDcwtM-AkRX2buCeI5e0E";
+
+            when(secretUtils.getTokenJwtSecret()).thenReturn(new TokenJwtSecret(TOKEN_SECRET, TOKEN_ISSUER));
+
+            assertThrows(JWTVerificationException.class, () -> tokenJwtUtils.readToken(expiredToken));
+        }
+
+        @Test
+        void readTokenInvalidSecretAndIssuer() {
+            String validToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBdWRpZW5jZSIsImlzcyI6IlRFU1QtSVNTVUVSIiwiaWQiOiJBQkMwMDEiLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5hbWUiOiJOb21lIiwidHlwZSI6MCwiaWF0IjoxNzA2NDEzNDMyLCJqdGkiOiJmMDQyM2U0Yy03YzEwLTRiMzUtOGQ5NS1kYmZhZmMzY2ZkYTQifQ._yL06ZjOO0k2Iin1udWAeou-MYVukcZ9XVOsJScVNiU";
+
+            when(secretUtils.getTokenJwtSecret()).thenReturn(new TokenJwtSecret("INVALID-SECRET", "INVALID-ISSUER"));
+
+            assertThrows(JWTVerificationException.class, () -> tokenJwtUtils.readToken(validToken));
+        }
     }
 
-    @Test
-    void readTokenExpired() {
-        String expiredToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBdWRpZW5jZSIsImNsaWVudElkIjoxLCJpc3MiOiJURVNULUlTU1VFUiIsImNwZiI6IjE2NTY1ODI0NzM4IiwiZXhwIjoxNjk4NjI4MjE2LCJpYXQiOjE2OTg2MjgxNTYsImp0aSI6ImRmYjM1OWFjLTI3MGItNDUxMC05YjcxLTZiYjE3YzI5MzQ1NCJ9.iVcSUx-UShDr1rSSJt2lweRDcwtM-AkRX2buCeI5e0E";
+    @Nested
+    class getFromToken {
+        @Test
+        void getEmployeeFromToken() {
+            var expected = new Employee("ABC001", "email@gmail.com", "Nome", EmployeeType.USER);
+            String validToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBdWRpZW5jZSIsImlzcyI6IlRFU1QtSVNTVUVSIiwiaWQiOiJBQkMwMDEiLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5hbWUiOiJOb21lIiwidHlwZSI6MCwiaWF0IjoxNzA2NDEzNDMyLCJqdGkiOiJmMDQyM2U0Yy03YzEwLTRiMzUtOGQ5NS1kYmZhZmMzY2ZkYTQifQ._yL06ZjOO0k2Iin1udWAeou-MYVukcZ9XVOsJScVNiU";
 
-        when(secretUtils.getTokenJwtSecret()).thenReturn(new TokenJwtSecret(TOKEN_SECRET, TOKEN_ISSUER));
+            when(secretUtils.getTokenJwtSecret()).thenReturn(new TokenJwtSecret(TOKEN_SECRET, TOKEN_ISSUER));
 
-        assertThrows(JWTVerificationException.class, () -> tokenJwtUtils.readToken(expiredToken));
-    }
+            var result = tokenJwtUtils.getEmployeeFromToken(validToken);
+            assertEquals(expected, result);
+        }
 
-    @Test
-    void readTokenInvalidSecretAndIssuer() {
-        String validToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBdWRpZW5jZSIsImlzcyI6IlRFU1QtSVNTVUVSIiwiaWQiOiJBQkMwMDEiLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5hbWUiOiJOb21lIiwidHlwZSI6MCwiaWF0IjoxNzA2NDEzNDMyLCJqdGkiOiJmMDQyM2U0Yy03YzEwLTRiMzUtOGQ5NS1kYmZhZmMzY2ZkYTQifQ._yL06ZjOO0k2Iin1udWAeou-MYVukcZ9XVOsJScVNiU";
+        @Test
+        void getAuthoritiesFromToken() {
+            var expected = List.of(new SimpleGrantedAuthority("0"));
+            String validToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBdWRpZW5jZSIsImlzcyI6IlRFU1QtSVNTVUVSIiwiaWQiOiJBQkMwMDEiLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5hbWUiOiJOb21lIiwidHlwZSI6MCwiaWF0IjoxNzA2NDEzNDMyLCJqdGkiOiJmMDQyM2U0Yy03YzEwLTRiMzUtOGQ5NS1kYmZhZmMzY2ZkYTQifQ._yL06ZjOO0k2Iin1udWAeou-MYVukcZ9XVOsJScVNiU";
 
-        when(secretUtils.getTokenJwtSecret()).thenReturn(new TokenJwtSecret("INVALID-SECRET", "INVALID-ISSUER"));
+            when(secretUtils.getTokenJwtSecret()).thenReturn(new TokenJwtSecret(TOKEN_SECRET, TOKEN_ISSUER));
 
-        assertThrows(JWTVerificationException.class, () -> tokenJwtUtils.readToken(validToken));
+            var result = tokenJwtUtils.getAuthoritiesFromToken(validToken);
+            assertEquals(expected, result);
+        }
     }
 }
